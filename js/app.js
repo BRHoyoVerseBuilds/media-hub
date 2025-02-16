9cd0c911d212ada9ada7cd388c6ebe9c0335ea8d
BReitan <brhoyoversebuilds@gmail.com>
2025-02-16 09:16:05 -0800

    Delete js directory

--- js/app.js
+++
@@ -1,9 +0,0 @@
-import { initializeTheme } from './theme.js';
-import { initializeLinks } from './links.js';
-import { initializeBookmarks } from './bookmarks.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initializeTheme();
-  initializeLinks();
-  initializeBookmarks();
-});
